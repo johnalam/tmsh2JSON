24b212e6039ec1536ee5d77ffe2c700070e49653
John Alam <joalam@verizon.net>
2019-01-08 18:43:30 -0500

    new updates
    Add support for the following:
    Export one iRule.  If more than one iRule exists, script may fail.
    Pool mentioned within irule are also exported.
    Fix issue where script failed if existing virtual did not have a WAF policy.

--- AS3_tmpl_export.tcl
+++ AS3_tmpl_export.tcl
@@ -11,7 +11,7 @@ foreach typ $p_typ {
     foreach profile [tmsh::get_config /ltm profile $typ] {
         set profile_name [tmsh::get_name $profile]
         set profile_type [string range [tmsh::get_type $profile] 12 end]
-        #puts "$profile_name $profile_type"
+        #puts "--> $profile_name $profile_type"
         set profs($profile_name) $profile_type
     }
 }
@@ -53,9 +53,9 @@ proc load_pool {pool_name decl} {
     set addresses [string trimright $addresses ,]
     set list_of_changes [list POOL_NAME_HERE $pool_name MONITOR_HERE $monitor RETRIES_HERE $retries LB_MODE_HERE $lb_method ACTION_DOWN_HERE $action SERVER_ADDRESSES_HERE $addresses PORT_HERE $port]
 
-    set pool_JSON [string map $list_of_changes $::pool_decl]
+    set pool_JSON "$pool_JSON,\n[string map $list_of_changes $::pool_decl]"
 
-
+    return pool_JSON
 }
 
 
@@ -78,14 +78,24 @@ proc get_addr_port {s ip prt} {
 }
 
 
+proc get_irule_json {rule} {  
+    set rule [regsub -all -line {\n}  $rule "\\n"]
 
+    regexp {^ltm rule (\S+) \{(.*)\}} $rule p0 rulename irulebody
+#    puts $rule
+#    puts $irulebody
+    set rule_json ",\n                   \"$rulename\": \{\n\"class\":\"iRule\",\n  \"iRule\":\"$irulebody\",\n\"expand\":\"true\"\}"
+    set rule_pointer ",\n                   \"iRules\": \"$rulename\""
+    return [list $rule_json $rule_pointer]
+
+}
 
 
 
 proc script::run {} {
 
 
-load_profile {tcp client-ssl server-ssl http web-security} profiles
+load_profile {tcp fastl4 client-ssl server-ssl http web-security} profiles
 load_WAF_pol waf_pol
 
 set AS3_host "BigIP"
@@ -96,13 +106,14 @@ if {$tmsh::argc >0 } {
     set virtual_to_export ""
 }
 
+regexp {(\S+)\/(\S+)}  [tmsh::get_name [lindex [tmsh::get_config /sys management-ip] 0]] foobar mgmt_IP mask
+set systemTime [clock seconds]
+set id [clock format $systemTime -format %m-%d-%Y_%H:%M:%S]
+
 
 
 foreach virt [tmsh::get_config /ltm virtual $virtual_to_export all-properties] {
-    set systemTime [clock seconds]
-    set id [clock format $systemTime -format %m-%d-%Y_%H:%M:%S]
-
-
+  if { [catch {
     set virt_name [tmsh::get_name $virt]
     set virt_dest_IP [tmsh::get_field_value $virt "destination"]
     puts "\n[tmsh::get_name $virt]   $virt_dest_IP\n"
@@ -111,7 +122,6 @@ foreach virt [tmsh::get_config /ltm virtual $virtual_to_export all-properties] {
     set port ""
     get_addr_port $virt_dest_IP IP port
 
-    regexp {(\S+)\/(\S+)}  [tmsh::get_name [lindex [tmsh::get_config /sys management-ip] 0]] foobar mgmt_IP mask
     set partition [tmsh::get_field_value $virt "partition"]
     set list_of_changes [list "VIRTUAL_NAME_HERE" $virt_name]
     lappend list_of_changes "DESTINATION_IP_HERE" $IP
@@ -119,16 +129,42 @@ foreach virt [tmsh::get_config /ltm virtual $virtual_to_export all-properties] {
 
     # Add pool name
     set pool_name  "[tmsh::get_field_value $virt pool]"
-    lappend list_of_changes "POOL_NAME_HERE" $pool_name
     set pool_JSON ""
-    load_pool $pool_name pool_JSON
+    if { $pool_name ne "none" } {
+        lappend list_of_changes "POOL_NAME_HERE" $pool_name
+        load_pool $pool_name pool_JSON
+    }
 
     lappend list_of_changes "ID_HERE" $id
     lappend list_of_changes "DESCRIPTION_HERE"  "$virt_name - [string map {\" " "} [tmsh::get_field_value $virt description]]"
     lappend list_of_changes "PORT_HERE" $port
 
+    set irule_json ""
+    set irule_ptr ""
+    set irule [tmsh::get_field_value $virt "rules"]
+    if { $irule ne "none" } {
+        set irule [lindex [tmsh::get_config ltm rule $irule] 0]
+        set i 0
+        foreach item [regexp -all -inline { pool (\S+)} $irule] {
+            if { $i == 1 } {
+                load_pool $item pool_JSON
+                puts $item
+                set i 0
+            } else {
+                set i 1
+            }
+        }
+
+       set irule  [get_irule_json $irule]
+       set irule_ptr  [lindex $irule 1]
+       set irule_json [lindex $irule 0]
+
+
+        #puts $irule_json
+    }
+    set wafpol_out ""
+ 
     foreach virt_profile [tmsh::get_field_value [lindex [tmsh::get_config /ltm virtual $virt_name profiles] 0] profiles] {
-#        puts  "[tmsh::get_name $virt_profile]   TYPE $profiles([tmsh::get_name $virt_profile])"
 
         if { [catch { set profil_type $profiles([tmsh::get_name $virt_profile]) } ] } {
             set string ""
@@ -145,6 +181,7 @@ foreach virt [tmsh::get_config /ltm virtual $virtual_to_export all-properties] {
                     }
                 }
             }
+            fastl4 {}
             http { set string "HTTP_PROFILE_HERE"
                 foreach feature [tmsh::get_config /ltm profile $profil_type $profil_name non-default-properties]  {
     #                puts "$feature"
@@ -153,8 +190,15 @@ foreach virt [tmsh::get_config /ltm virtual $virtual_to_export all-properties] {
             client-ssl { set string "CLIENT_SSL_PROFILE_HERE" }
             server-ssl { set string "SERVER_SSL_PROFILE_HERE" }
             web-security { set string "LTM_POLICY_HERE" }
-            default { set string "WAF_POLICY_HERE"
-                        set profil_name "/Common/$waf_pol($virt_name)"
+            default {
+                if { [info exists waf_pol($virt_name)] } {
+                     set string "WAF_POLICY_HERE"
+                     set profil_name "/Common/$waf_pol($virt_name)"
+                     if { $waf_pol($virt_name) ne "" } {
+                        set l [list WAF_POLICY_HERE /Common/$waf_pol($virt_name)]
+                        set wafpol_out ,\n[string map $l  $::WAF_pol]
+                     }
+                }
             }
         }
         lappend list_of_changes $string $profil_name
@@ -162,6 +206,7 @@ foreach virt [tmsh::get_config /ltm virtual $virtual_to_export all-properties] {
     }
 
     #puts $list_of_changes
+
 
 
     set new_declaration [string trimright [string map $list_of_changes $::declaration] \}\}\}\}]
@@ -176,15 +221,12 @@ foreach virt [tmsh::get_config /ltm virtual $virtual_to_export all-properties] {
         set logpol_out ,\n$logpol_out
     }
 
-    set wafpol_out ""
-    if { $waf_pol($virt_name) ne "" } {
-        set l [list WAF_POLICY_HERE /Common/$waf_pol($virt_name)]
-        set wafpol_out ,\n[string map $l  $::WAF_pol]
-    }
-    puts [string trimright $new_declaration \}]$wafpol_out$logpol_out\},\n$pool_JSON\n\}\n\}\n\}\n\}
+   puts [string trimright $new_declaration$irule_ptr \}]$wafpol_out$irule_json$logpol_out\}$pool_JSON\n\}\n\}\n\}\n\}
 
     puts "--------------------------------------------------------------------------"
-
+  }] } {
+      puts "Error with [tmsh::get_name $virt]."
+  }
 
 }
 
@@ -240,8 +282,7 @@ proc script::init {} {
                     "members": [{
                             "servicePort": PORT_HERE,
                             "serverAddresses": [ SERVER_ADDRESSES_HERE ] }  ]
-                    }
-                }
+                    } }
 
 
 
@@ -259,6 +300,5 @@ proc script::init {} {
 
 
 }
-    
-}
 
+}
